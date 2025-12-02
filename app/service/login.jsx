a248c3076818df35f680807ebd88e1831c998d8a
CaioCintra <caiocintra2000@gmail.com>
2025-12-02 14:08:54 -0300

    Front deploy

--- app/service/login.jsx
+++ app/service/login.jsx
@@ -1,28 +1,6 @@
-const getHost = () => {
-  if (typeof window === "undefined") {
-    return process.env.NEXT_PUBLIC_API_URL ? "producao" : "validacao";
-  }
-
-  const url = window.location.origin;
-  if (url.includes("localhost")) {
-    return "validacao";
-  } else if (url.includes(process.env.NEXT_PUBLIC_SYSTEM_URL)) {
-    return "producao";
-  } else {
-    return "invalid";
-  }
+export const apiConst = {
+  api: process.env.NEXT_PUBLIC_API_URL,
 };
-
-const host = getHost();
-
-export const apiConst =
-  host === "producao"
-    ? {
-        api: process.env.NEXT_PUBLIC_API_URL,
-      }
-    : {
-        api: process.env.NEXT_PUBLIC_API_URL,
-      };
 
 export const authUser = async (name, password) => {
   try {
